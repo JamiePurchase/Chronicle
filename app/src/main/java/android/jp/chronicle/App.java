758c619bc4f9d481a3ac53a6e47a281ee9beb6ca
Jamie Purchase <jamie.purchase@medicapp.com>
2015-04-26 09:28:14 +0100

    Added Volley for Web Requests

--- app/src/main/java/android/jp/chronicle/App.java
+++ app/src/main/java/android/jp/chronicle/App.java
@@ -1,11 +1,19 @@
 package android.jp.chronicle;
 
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
@@ -35,14 +43,37 @@ public class App extends Activity
 
         setContentView(new AppPanel(this));
 
-        HttpClient client = new DefaultHttpClient();
+        /*HttpClient client = new DefaultHttpClient();
         HttpPost post = new HttpPost("http://chronicle.co.nf/test1.php");
 
         List<NameValuePair> pairs = new ArrayList<NameValuePair>();
         pairs.add(new BasicNameValuePair("key1", "value1"));
         pairs.add(new BasicNameValuePair("key2", "value2"));
         try {post.setEntity(new UrlEncodedFormEntity(pairs));}
-        catch (UnsupportedEncodingException e){}
+        catch (UnsupportedEncodingException e){}*/
+
+        // Attempting to make a web request with Volley
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //mTextView.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
 
